d547f7197459ced0d9cb5db3d5861ad1f31f2043
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-06-28 08:49:04 +0500

    project: add empty roadmap page

---
+++ frontend/src/components/project/detail/ProjectRoadmap.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>roadmap</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'ProjectRoadmap',
+
+  components: {},
+
+  props: {},
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="sass" scoped></style>
