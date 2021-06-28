99fe9654d811d7bd5f18b8e6a35fcea29bf2c8df
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-06-28 10:00:10 +0500

    common: fix useless await word

--- frontend/src/components/common/CommonClipboard.vue
+++ frontend/src/components/common/CommonClipboard.vue
@@ -25,7 +25,7 @@ export default defineComponent({
 
     async function copyToClipboard() {
       await navigator.clipboard.writeText(props.text);
-      await q.notify({
+      q.notify({
         message: 'Copied to clipboard',
         type: 'info',
         textColor: 'white',
