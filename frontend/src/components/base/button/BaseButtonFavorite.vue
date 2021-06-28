6acf145b1d4a419a942b46438de728b4be3dc4c0
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-06-28 08:42:51 +0500

    base: fix favorite button, add tooltip

--- frontend/src/components/base/button/BaseButtonFavorite.vue
+++ frontend/src/components/base/button/BaseButtonFavorite.vue
@@ -1,29 +1,56 @@
 <template>
   <q-btn
-    size="md"
+    :size="size"
     :icon="favorite ? 'star' : 'star_border'"
     :color="favorite ? 'orange-5' : 'grey-6'"
-    flat
-    round
+    :flat="flat"
+    :round="round"
     v-bind="$attrs"
-  />
+  >
+    <BaseTooltip :label="favorite ? tooltipRemove : tooltipAdd" />
+  </q-btn>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BaseTooltip from 'components/base/BaseTooltip.vue';
 
 export default defineComponent({
   name: 'BaseButtonFavorite',
+
+  components: { BaseTooltip },
 
   props: {
     favorite: {
       type: Boolean,
       required: true,
     },
+
+    size: {
+      type: String,
+      required: false,
+      default: 'md',
+    },
+    flat: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
     round: {
       type: Boolean,
       required: false,
       default: true,
+    },
+
+    tooltipRemove: {
+      type: String,
+      required: false,
+      default: 'Снять отметку',
+    },
+    tooltipAdd: {
+      type: String,
+      required: false,
+      default: 'Пометить',
     },
   },
 });
