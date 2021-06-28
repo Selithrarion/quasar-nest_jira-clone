584229e31386b514ed85f7fd39ef79224fd48c46
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-06-28 08:44:06 +0500

    common: add clipboard component

---
+++ frontend/src/components/common/CommonClipboard.vue
@@ -0,0 +1,41 @@
+<template>
+  <span @click="copyToClipboard">
+    <slot>
+      <q-icon name="content-copy" />
+    </slot>
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useQuasar } from 'quasar';
+
+export default defineComponent({
+  name: 'CommonClipboard',
+
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const q = useQuasar();
+
+    async function copyToClipboard() {
+      await navigator.clipboard.writeText(props.text);
+      await q.notify({
+        message: 'Copied to clipboard',
+        type: 'info',
+        textColor: 'white',
+        timeout: 1000,
+      });
+    }
+
+    return {
+      copyToClipboard,
+    };
+  },
+});
+</script>
