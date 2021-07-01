3517d1054487f58346bd0e6e0708c466be1269d3
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-01 12:33:30 +0500

    base: refactor close button

--- frontend/src/components/base/button/BaseButtonCloseIcon.vue
+++ frontend/src/components/base/button/BaseButtonCloseIcon.vue
@@ -1,10 +1,29 @@
 <template>
-  <q-btn v-close-popup icon="close" flat round dense />
+  <q-btn v-close-popup icon="close" :flat="flat" :round="round" :dense="dense" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+
 export default defineComponent({
   name: 'BaseButtonCloseIcon',
+
+  props: {
+    round: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    flat: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    dense: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  },
 });
 </script>
