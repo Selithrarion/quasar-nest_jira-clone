16710843fbe4a2d774d075416ac6d7d27b5717da
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-01 12:36:41 +0500

    project-board: add column wrapper

---
+++ frontend/src/components/project/board/ProjectBoardColumnWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row justify-between gap-4 no-wrap">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ProjectBoardColumnWrapper',
+});
+</script>
