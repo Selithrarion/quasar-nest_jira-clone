7a710d29c1ea1ecb9bd1065bb0104c38b5c82f11
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-04 14:18:51 +0500

    b-global: init

---
+++ backend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+    jest: true,
+  },
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+
+  rules: {
+    // '@typescript-eslint/interface-name-prefix': 'off',
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
