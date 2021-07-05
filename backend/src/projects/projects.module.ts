e8e88c61dc617271c5e53894143e9a23988eb024
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-05 14:20:48 +0500

    b-project: add project module

---
+++ backend/src/projects/projects.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ProjectsController } from './projects.controller';
+import { ProjectsService } from './projects.service';
+import { ProjectEntity } from './entity/project.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ProjectEntity])],
+  controllers: [ProjectsController],
+  providers: [ProjectsService],
+})
+export class ProjectsModule {}
