e48c587b1376d8d185d7d91cd556b1cc4224aba7
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-06 12:12:07 +0500

    b-global: add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Jira API')
+    .setDescription('Jira API description')
+    .setVersion('1.0')
+    .addTag('jira')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors()
+
   await app.listen(3000);
 }
 bootstrap();
