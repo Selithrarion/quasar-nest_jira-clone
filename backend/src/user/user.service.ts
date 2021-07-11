86868966c3cb8d245e140ca313a1a99f07dc9c26
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-11 11:44:53 +0500

    b-user: add user service

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDTO } from './dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly users: Repository<UserEntity>
+  ) {}
+
+  async getByEmail(email: string): Promise<UserEntity> {
+    return await this.users.findOneOrFail({ email });
+  }
+
+  async getById(id: number): Promise<UserEntity> {
+    return await this.users.findOneOrFail({ id });
+  }
+
+  async create(userData: CreateUserDTO): Promise<UserEntity> {
+    const createdUser = await this.users.create(userData);
+    await this.users.save(createdUser);
+    return createdUser;
+  }
+}
