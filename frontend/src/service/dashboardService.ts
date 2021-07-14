4d5ee86e0a3bf89b202505017fff4210e9046baf
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-14 18:58:39 +0500

    f-dashboard: fix api responses

--- frontend/src/service/dashboardService.ts
+++ frontend/src/service/dashboardService.ts
@@ -1,18 +1,23 @@
 import { http } from 'boot/axios';
 import { DashboardDTO, DashboardModel } from 'src/models/dashboard/dashboard.model';
+import { ApiResponseModel } from 'src/models/common/apiResponse.model';
 
 export default {
   async getAll(): Promise<DashboardModel[]> {
-    return await http.get('/dashboard');
+    const { data }: ApiResponseModel = await http.get('/dashboard');
+    return data as DashboardModel[];
   },
   async getByID(id: number): Promise<DashboardModel> {
-    return await http.get(`/dashboard/${id}`);
+    const { data }: ApiResponseModel = await http.get(`/dashboard/${id}`);
+    return data as DashboardModel;
   },
   async create(dashboardData: DashboardDTO): Promise<DashboardModel> {
-    return await http.post('/dashboard', dashboardData);
+    const { data }: ApiResponseModel = await http.post('/dashboard', dashboardData);
+    return data as DashboardModel;
   },
   async update(id: number, dashboardData: Partial<DashboardDTO>): Promise<DashboardModel> {
-    return await http.patch(`/dashboard/${id}`, dashboardData);
+    const { data }: ApiResponseModel = await http.patch(`/dashboard/${id}`, dashboardData);
+    return data as DashboardModel;
   },
   async delete(id: number): Promise<void> {
     return await http.delete(`/dashboard/${id}`);
