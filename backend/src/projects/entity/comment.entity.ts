67ee157fa512f5db5a20321f7fa09c92014da859
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-14 18:56:14 +0500

    global: fix comment model

--- backend/src/projects/entity/comment.entity.ts
+++ backend/src/projects/entity/comment.entity.ts
@@ -1,11 +1,18 @@
-import { Column } from 'typeorm';
-import { UserEntity } from '../../user/entity/user.entity';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { BaseEntity } from '../../common/base.entity';
+import { UserEntity } from '../../user/entity/user.entity';
+import { IssueEntity } from './issue.entity';
 
+@Entity()
 export class CommentEntity extends BaseEntity {
   @Column()
+  text: string;
+
+  @ManyToOne(() => UserEntity)
+  @JoinColumn({ name: 'authorID' })
   author: UserEntity;
 
-  @Column()
-  text: string;
+  @ManyToOne(() => IssueEntity, (issue) => issue.comments)
+  @JoinColumn({ name: 'issueID' })
+  issue: IssueEntity;
 }
--- frontend/src/models/project/comment.model.ts
+++ frontend/src/models/project/comment.model.ts
@@ -1,7 +1,9 @@
+import { BaseModel } from 'src/models/common/base.model';
 import { UserModel } from 'src/models/user/user.model';
+import { IssueModel } from 'src/models/project/issue.model';
 
-export interface CommentModel {
-  readonly id: number;
-  author: UserModel;
+export interface CommentModel extends BaseModel {
   text: string;
+  author: UserModel;
+  issue: IssueModel;
 }
