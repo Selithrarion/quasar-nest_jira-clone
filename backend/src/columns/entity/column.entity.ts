ba49af586836c9f3ddfbfea2060e1a2db8552317
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-22 08:22:11 +0500

    b-columns: fix ColumnEntity cascade

--- backend/src/columns/entity/column.entity.ts
+++ backend/src/columns/entity/column.entity.ts
@@ -1,14 +1,17 @@
-import {Column, Entity, JoinColumn, ManyToOne, OneToMany} from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { BaseEntity } from '../../common/base.entity';
-import { IssueEntity } from './issue.entity';
 import { BoardEntity } from '../../boards/entity/board.entity';
+import { IssueEntity } from './issue.entity';
 
 @Entity()
 export class ColumnEntity extends BaseEntity {
   @Column()
   name: string;
 
-  @ManyToOne(() => BoardEntity, (board) => board.columns)
+  @ManyToOne(() => BoardEntity, (board) => board.columns, {
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
   @JoinColumn({ name: 'boardID' })
   board: BoardEntity;
 
