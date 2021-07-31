aeb01ec83c06b36e664f814ea68e739ee0f36ade
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-31 14:49:10 +0500

    b-auth: add local strategy

---
+++ backend/src/auth/local.strategy.ts
@@ -0,0 +1,18 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserEntity } from '../user/entity/user.entity';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(username: string, password: string): Promise<UserEntity | UnauthorizedException> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
+}
