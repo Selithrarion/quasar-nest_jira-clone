73027344e93bc668f9bafc83ea06c59602aa93c1
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-31 14:50:19 +0500

    b-auth: add login endpoint, import passport

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
