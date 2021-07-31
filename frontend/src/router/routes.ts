af3b7f149cdcd15409430b3606d5436ad10c06ec
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-07-31 14:48:51 +0500

    f-global: add auth meta to routes, add auth guest route

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -2,15 +2,33 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
+    path: '/auth',
+    component: () => import('layouts/GuestLayout.vue'),
+    children: [
+      {
+        path: '/auth',
+        component: () => import('pages/Auth.vue'),
+        meta: { guest: true },
+      },
+    ],
+  },
+
+  {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '/', component: () => import('pages/Index.vue') },
+      { path: '/', component: () => import('pages/Index.vue'), meta: { auth: true } },
 
-      { path: '/projects', name: 'projects', component: () => import('pages/project/Projects.vue') },
+      {
+        path: '/projects',
+        name: 'projects',
+        component: () => import('pages/project/Projects.vue'),
+        meta: { auth: true },
+      },
       {
         path: '/projects/:projectID',
         component: () => import('pages/project/ProjectDetail.vue'),
+        meta: { auth: true },
         children: [
           {
             path: 'board/:boardID',
@@ -31,19 +49,21 @@ const routes: RouteRecordRaw[] = [
         ],
       },
 
-      { path: 'filters', component: () => import('pages/Filters.vue') },
+      { path: 'filters', component: () => import('pages/Filters.vue'), meta: { auth: true } },
 
       {
         path: 'dashboards',
         component: () => import('pages/dashboard/Dashboard.vue'),
+        meta: { auth: true },
       },
       {
         path: 'dashboards/:id',
         component: () => import('pages/dashboard/DashboardDetail.vue'),
+        meta: { auth: true },
       },
 
-      { path: 'people', component: () => import('pages/people/People.vue') },
-      { path: 'people/team/:id', component: () => import('pages/people/PeopleTeam.vue') },
+      { path: 'people', component: () => import('pages/people/People.vue'), meta: { auth: true } },
+      { path: 'people/team/:id', component: () => import('pages/people/PeopleTeam.vue'), meta: { auth: true } },
       // { path: 'apps', component: () => import('pages/Apps.vue') },
     ],
   },
