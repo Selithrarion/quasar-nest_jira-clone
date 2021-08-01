e6b364dda558cf6c09cd433e0387a1bdfadf3edb
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-01 20:01:35 +0500

    f-global: add router beforeEach auth check

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,17 +1,16 @@
 import { route } from 'quasar/wrappers';
-import {
-  createMemoryHistory,
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from 'vue-router';
+import { createMemoryHistory, createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 import { StateInterface } from '../store';
+import userState from '../store/user/state';
+
 import routes from './routes';
 
 export default route<StateInterface>(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -20,9 +19,36 @@ export default route<StateInterface>(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(
-      process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE
-    ),
+    history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE),
+  });
+
+  const isUserLogged = Boolean(userState().token);
+  const redirectToAuthPage = {
+    path: '/auth',
+  };
+
+  Router.beforeEach((to, from, next) => {
+    const matchedRoutes = to.matched;
+    const isNeedAuth = matchedRoutes.some((record) => record.meta.auth);
+    const isNeedGuest = matchedRoutes.some((record) => record.meta.guest);
+
+    if (isNeedAuth) {
+      if (!isUserLogged) next(redirectToAuthPage);
+      else next();
+    } else if (isNeedGuest) {
+      console.log('isNeedGuest', isUserLogged);
+      next();
+      // if (false) {
+      //   next({
+      //     path: '/',
+      //     query: {
+      //       redirect: to.fullPath,
+      //     },
+      //   });
+      // }
+    } else {
+      next(); // make sure to always call next()!
+    }
   });
 
   return Router;
