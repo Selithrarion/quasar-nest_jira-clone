962e132018aaa30fdaebd7472d696dac0dec2f1c
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-01 20:00:38 +0500

    f-user: add login/register vuex actions

--- frontend/src/store/user/actions.ts
+++ frontend/src/store/user/actions.ts
@@ -1,7 +1,19 @@
 import { ActionTree } from 'vuex';
 import { StateInterface } from '../index';
 import { UserStateInterface } from 'src/store/user/state';
+import { UserLoginDTO, UserRegisterDTO } from 'src/models/user/user.model';
+import userService from 'src/service/userService';
 
-const actions: ActionTree<UserStateInterface, StateInterface> = {};
+const actions: ActionTree<UserStateInterface, StateInterface> = {
+  async login({ commit }, payload: UserLoginDTO) {
+    const data = await userService.login(payload);
+    console.log('LOGIN', data);
+    commit('AUTH_USER', data);
+  },
+  async register({ commit }, payload: UserRegisterDTO) {
+    const data = await userService.register(payload);
+    commit('AUTH_USER', data);
+  },
+};
 
 export default actions;
--- frontend/src/store/user/mutations.ts
+++ frontend/src/store/user/mutations.ts
@@ -1,6 +1,12 @@
 import { MutationTree } from 'vuex';
 import { UserStateInterface } from './state';
+import { UserAuthResponse } from 'src/models/user/user.model';
 
-const mutation: MutationTree<UserStateInterface> = {};
+const mutation: MutationTree<UserStateInterface> = {
+  AUTH_USER(state: UserStateInterface, { user, token }: UserAuthResponse) {
+    state.currentUser = user;
+    state.token = token;
+  },
+};
 
 export default mutation;
