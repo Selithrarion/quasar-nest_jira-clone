b02e7430730ba2eb77b862b391da22224ec54f8e
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-01 19:59:45 +0500

    f-user: fix userService, add update tokens req

--- frontend/src/service/userService.ts
+++ frontend/src/service/userService.ts
@@ -1,11 +1,11 @@
 import { http } from 'boot/axios';
-import { LoginDTO, RegisterDTO } from 'src/models/user/user.model';
+import { UserAuthResponse, UserLoginDTO, UserRegisterDTO, UserUpdateTokenResponse } from 'src/models/user/user.model';
 
 export default {
-  async login(payload: LoginDTO): Promise<void> {
+  async login(payload: UserLoginDTO): Promise<UserAuthResponse> {
     return await http.post('/auth/login', payload);
   },
-  async register(payload: RegisterDTO): Promise<void> {
+  async register(payload: UserRegisterDTO): Promise<UserAuthResponse> {
     return await http.post('/auth/register', payload);
   },
   async forgotPassword(email: string): Promise<void> {
@@ -13,5 +13,9 @@ export default {
   },
   async logout(): Promise<void> {
     return await http.post('/auth/logout');
+  },
+
+  async updateTokens(): Promise<UserUpdateTokenResponse> {
+    return await http.post('/auth/token');
   },
 };
