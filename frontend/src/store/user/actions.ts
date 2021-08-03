633579db7a530d2d6241feced65d6f2e1cff7cdc
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-03 14:36:33 +0500

    f-user: add logout action, add cookies token

--- frontend/src/store/user/actions.ts
+++ frontend/src/store/user/actions.ts
@@ -7,12 +7,15 @@ import userService from 'src/service/userService';
 const actions: ActionTree<UserStateInterface, StateInterface> = {
   async login({ commit }, payload: UserLoginDTO) {
     const data = await userService.login(payload);
-    console.log('LOGIN', data);
     commit('AUTH_USER', data);
   },
   async register({ commit }, payload: UserRegisterDTO) {
     const data = await userService.register(payload);
     commit('AUTH_USER', data);
+  },
+  async logout({ commit }) {
+    await userService.logout();
+    commit('LOGOUT_USER');
   },
 };
 
--- frontend/src/store/user/mutations.ts
+++ frontend/src/store/user/mutations.ts
@@ -1,11 +1,19 @@
 import { MutationTree } from 'vuex';
+import { Cookies } from 'quasar';
 import { UserStateInterface } from './state';
 import { UserAuthResponse } from 'src/models/user/user.model';
 
 const mutation: MutationTree<UserStateInterface> = {
-  AUTH_USER(state: UserStateInterface, { user, token }: UserAuthResponse) {
+  AUTH_USER(state: UserStateInterface, { user, accessToken }: UserAuthResponse) {
     state.currentUser = user;
-    state.token = token;
+    state.token = accessToken;
+    Cookies.set('user', JSON.stringify({ ...user, accessToken }));
+    // TODO: add refresh tokens
+  },
+  LOGOUT_USER(state: UserStateInterface) {
+    state.currentUser = null;
+    state.token = null;
+    Cookies.remove('user');
   },
 };
 
