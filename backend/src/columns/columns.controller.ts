fe03d19e80db3fc741580fbe9fb08a9346c2fb29
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-04 16:26:04 +0500

    b-global: fix swagger response docs

--- backend/src/columns/columns.controller.ts
+++ backend/src/columns/columns.controller.ts
@@ -10,7 +10,7 @@ export class ColumnsController {
 
   @ApiOperation({ summary: 'Create column' })
   @ApiResponse({ status: 201, description: 'Column was created' })
-  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Post()
   async create(@Body() columnData: CreateColumnDTO): Promise<ColumnEntity> {
     return await this.columnsService.create(columnData);
@@ -18,7 +18,7 @@ export class ColumnsController {
 
   @ApiOperation({ summary: 'Update column' })
   @ApiResponse({ status: 201, description: 'Column was updated' })
-  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Patch(':id')
   async update(@Param('id') id: number, @Body() columnData: UpdateColumnDTO): Promise<ColumnEntity> {
     return await this.columnsService.update(id, columnData);
@@ -26,7 +26,7 @@ export class ColumnsController {
 
   @ApiOperation({ summary: 'Delete column' })
   @ApiResponse({ status: 201, description: 'Column was deleted' })
-  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Delete(':id')
   async delete(@Param('id') id: number): Promise<void> {
     return await this.columnsService.delete(id);
--- backend/src/issues/issues.controller.ts
+++ backend/src/issues/issues.controller.ts
@@ -12,6 +12,7 @@ export class IssuesController {
 
   @ApiOperation({ summary: 'Get issue by ID' })
   @ApiResponse({ status: 200, description: 'Return issue' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Get(':id')
   async getByID(@Param('id') id: number): Promise<IssueEntity> {
     return await this.issuesService.getByID(id);
@@ -19,7 +20,7 @@ export class IssuesController {
 
   @ApiOperation({ summary: 'Create issue' })
   @ApiResponse({ status: 201, description: 'Issue was created' })
-  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Post()
   async create(@Body() payload: CreateIssueDTO): Promise<IssueEntity> {
     return await this.issuesService.create(payload);
@@ -27,7 +28,7 @@ export class IssuesController {
 
   @ApiOperation({ summary: 'Update issue' })
   @ApiResponse({ status: 201, description: 'Issue was updated' })
-  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Patch(':id')
   async update(@Param('id') id: number, @Body() payload: UpdateIssueDTO): Promise<IssueEntity> {
     return await this.issuesService.update(id, payload);
@@ -35,7 +36,7 @@ export class IssuesController {
 
   @ApiOperation({ summary: 'Delete issue' })
   @ApiResponse({ status: 201, description: 'Issue was deleted' })
-  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Delete(':id')
   async delete(@Param('id') id: number): Promise<void> {
     return await this.issuesService.delete(id);
