1e1763c2ee7066d720c79ba909aba314ec7ffa30
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-06 14:12:30 +0500

    base-loading: add center prop

--- frontend/src/components/base/BaseLoader.vue
+++ frontend/src/components/base/BaseLoader.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="base-loader text-center" :style="[pageMargin ? 'margin-top: 128px' : '']">
+  <div
+    class="base-loader text-center"
+    :style="[pageMargin ? 'margin-top: 128px' : '']"
+    :class="{ 'flex-center full-height': center }"
+  >
     <q-circular-progress size="40px" color="primary" :thickness="0.11" indeterminate v-bind="$attrs" />
   </div>
 </template>
@@ -14,6 +18,7 @@ export default defineComponent({
 
   props: {
     pageMargin: Boolean,
+    center: Boolean,
   },
 });
 </script>
