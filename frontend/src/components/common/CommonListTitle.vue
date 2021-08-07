91415f8869670e5fbf9dec7c63d315b3c124be8e
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-07 08:59:55 +0500

    f-common: add list title

---
+++ frontend/src/components/common/CommonListTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-caption text-uppercase" :class="{ 'q-py-xs q-px-md': padding }">
+    <slot>
+      {{ title }}
+    </slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CommonListTitle',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    padding: Boolean,
+  },
+});
+</script>
