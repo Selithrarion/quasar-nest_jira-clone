facec14f731b8d5e7d650eb6b341d73fd190c94f
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-07 08:59:44 +0500

    f-base: add tooltip to base close button

--- frontend/src/components/base/button/BaseButtonCloseIcon.vue
+++ frontend/src/components/base/button/BaseButtonCloseIcon.vue
@@ -1,12 +1,19 @@
 <template>
-  <q-btn v-close-popup icon="close" :flat="flat" :round="round" :dense="dense" />
+  <q-btn v-close-popup icon="close" :flat="flat" :round="round" :dense="dense">
+    <BaseTooltip label="Закрыть" />
+  </q-btn>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BaseTooltip from 'components/base/BaseTooltip.vue';
 
 export default defineComponent({
   name: 'BaseButtonCloseIcon',
+
+  components: {
+    BaseTooltip,
+  },
 
   props: {
     round: {
