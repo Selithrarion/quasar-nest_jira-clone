d7cfed6e66b4aca88cd2f151626c98abffd88511
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-14 11:27:04 +0500

    f-project-issue: fix comment model

--- frontend/src/models/project/comment.model.ts
+++ frontend/src/models/project/comment.model.ts
@@ -1,9 +1,7 @@
 import { BaseModel } from 'src/models/common/base.model';
 import { UserModel } from 'src/models/user/user.model';
-import { IssueModel } from 'src/models/project/issue.model';
 
 export interface CommentModel extends BaseModel {
   text: string;
   author: UserModel;
-  issue: IssueModel;
 }
