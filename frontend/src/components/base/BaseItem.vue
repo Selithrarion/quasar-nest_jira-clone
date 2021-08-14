47063181810c491b380214533d01ef198658d50e
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-14 19:25:00 +0500

    f-base: add BaseItem

---
+++ frontend/src/components/base/BaseItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-item :clickable="clickable" v-bind="$attrs">
+    <BaseLoader v-if="loading" class="flex-center" gray-color small />
+
+    <slot v-else>
+      <q-item-section>
+        <q-item-label>
+          {{ label }}
+        </q-item-label>
+      </q-item-section>
+    </slot>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BaseItem',
+
+  props: {
+    label: {
+      type: [Number, String],
+      required: false,
+      default: null,
+    },
+    clickable: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+
+    loading: Boolean,
+  },
+});
+</script>
