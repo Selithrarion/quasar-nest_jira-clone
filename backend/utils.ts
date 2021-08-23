2c29f086725a4691d89fa053e34fb224783f6497
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-23 08:36:15 +0500

    b-global: add utils file with stringToHslColor func

---
+++ backend/utils.ts
@@ -0,0 +1,9 @@
+export function stringToHslColor(string: string, saturation = 50, lightness = 80): string {
+  let hash = 0;
+  for (let i = 0; i < string.length; i++) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const hue = hash % 360;
+  return 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%)';
+}
