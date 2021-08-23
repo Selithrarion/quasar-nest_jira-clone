c498b7ee13dc66a53195c7a4c5716f867d166df6
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-23 08:35:59 +0500

    b-global: add aws config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,23 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { json, urlencoded } from 'express';
+import { config as AwsConfig } from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Jira API')
     .setDescription('Jira API description')
     .setVersion('1.0')
     .addTag('jira')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  const configService = app.get(ConfigService);
+  AwsConfig.update({
+    accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
+    region: configService.get('AWS_REGION'),
+  });
 
   await app.listen(process.env.PORT);
 }
