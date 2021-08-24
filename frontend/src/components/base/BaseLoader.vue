beea86437df0bf1a01bc86b5820a423912140533
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-24 07:52:31 +0500

    f-base-loading: add color and thickness props

--- frontend/src/components/base/BaseLoader.vue
+++ frontend/src/components/base/BaseLoader.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="base-loader text-center"
+    class="base-loader text-center z-top"
     :style="[pageMargin ? 'margin-top: 128px' : '']"
-    :class="{ 'flex-center full-height': center }"
+    :class="{ 'flex-center absolute-full': center }"
   >
     <q-circular-progress
       :size="computedSize"
-      :color="grayColor ? 'blue-grey-6' : 'primary'"
-      :thickness="0.11"
+      :color="computedColor"
+      :thickness="thickness"
       indeterminate
       v-bind="$attrs"
     />
@@ -27,6 +27,17 @@ export default defineComponent({
     center: Boolean,
     grayColor: Boolean,
 
+    color: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    thickness: {
+      type: [Number, String],
+      required: false,
+      default: 0.11,
+    },
+
     small: Boolean,
     medium: Boolean,
     large: {
@@ -42,9 +53,14 @@ export default defineComponent({
       else if (props.medium) return '24px';
       else return '40px';
     });
+    const computedColor = computed(() => {
+      if (props.color) return props.color;
+      else return props.grayColor ? 'blue-grey-6' : 'primary';
+    });
 
     return {
       computedSize,
+      computedColor,
     };
   },
 });
