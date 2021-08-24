1442956375da83443fd40bf051b15025cca9fc63
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-24 07:54:22 +0500

    b-user: add upload avatar and header endpoints

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,10 +1,25 @@
-import { Body, Controller, Get, Param, Patch, Query, Request } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Param,
+  Query,
+  Response,
+  Request,
+  UploadedFile,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { UserEntity } from './entity/user.entity';
 import { UserService } from './user.service';
 
 import { Public } from '../auth/decorators/public.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { PublicFileEntity } from '../files/entity/public-file.entity';
 
 @ApiTags('user')
 @Controller('user')
@@ -26,6 +41,20 @@ export class UserController {
   @Patch(':id')
   async update(@Param('id') id: number, @Body() payload: Partial<UserEntity>): Promise<UserEntity> {
     return await this.userService.update(id, payload);
+  }
+
+  @ApiBearerAuth()
+  @Post('upload-avatar')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadAvatar(@UploadedFile() file: Express.Multer.File, @Request() req): Promise<PublicFileEntity> {
+    return await this.userService.setUserImage(file, 'avatar', req.user.id);
+  }
+
+  @ApiBearerAuth()
+  @Post('upload-header')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadHeader(@UploadedFile() file: Express.Multer.File, @Request() req): Promise<PublicFileEntity> {
+    return await this.userService.setUserImage(file, 'header', req.user.id);
   }
 
   @Public()
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -5,8 +5,10 @@ import { UserEntity } from './entity/user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
+import { FilesModule } from '../files/files.module';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), FilesModule],
   exports: [UserService],
   providers: [UserService],
   controllers: [UserController],
