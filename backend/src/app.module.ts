3204e45edca6854e9dbc22ed9f9604278ddff9e4
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-26 13:11:01 +0500

    global: add docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -25,7 +25,7 @@ import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
       port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      database: process.env.POSTGRES_DB,
       entities: ['dist/**/entity/*.entity{.ts,.js}'],
       synchronize: true,
     }),
