d76f8202f67d896f77ac829492aca961317b71e4
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-08-31 19:55:55 +0500

    b-user: add search service

---
+++ backend/src/modules/user/user-search.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@nestjs/common';
+import { UserEntity } from './entity/user.entity';
+import { ApiResponse } from '@elastic/elasticsearch';
+import { UserSearchBody, UserSearchResult } from './types/user-search';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+
+@Injectable()
+export class UserSearchService {
+  private readonly index = 'user';
+
+  constructor(private readonly elasticService: ElasticsearchService) {}
+
+  async indexUser(user: UserEntity): Promise<ApiResponse> {
+    return this.elasticService.index<UserSearchResult, UserSearchBody>({
+      index: this.index,
+      body: {
+        id: user.id,
+        username: user.username,
+      },
+    });
+  }
+
+  async search(searchText: string): Promise<UserSearchBody[]> {
+    const { body } = await this.elasticService.search<UserSearchResult>({
+      index: this.index,
+      body: {
+        query: {
+          match: {
+            username: {
+              query: searchText,
+            },
+          },
+        },
+      },
+    });
+    console.log(body);
+    const hits = body.hits.hits;
+    return hits.map((item) => item._source);
+  }
+
+  async update(user: UserEntity): Promise<ApiResponse> {
+    const body: UserSearchBody = {
+      id: user.id,
+      username: user.username,
+    };
+    const script = Object.entries(body).reduce((result, [key, value]) => {
+      return `${result} ctx._source.${key}='${value}';`;
+    }, '');
+
+    return this.elasticService.updateByQuery({
+      index: this.index,
+      body: {
+        query: {
+          match: {
+            id: user.id,
+          },
+        },
+        script: {
+          inline: script,
+        },
+      },
+    });
+  }
+
+  async remove(id: number): Promise<void> {
+    this.elasticService.deleteByQuery({
+      index: this.index,
+      body: {
+        query: {
+          match: {
+            id,
+          },
+        },
+      },
+    });
+  }
+}
