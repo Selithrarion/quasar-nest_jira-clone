0e7f7e83b442140f560fa77afc10222b05303bd0
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-08 19:35:20 +0500

    f-project-issues: add issue markup proto

--- frontend/src/components/project/issues/ProjectIssues.vue
+++ frontend/src/components/project/issues/ProjectIssues.vue
@@ -1,19 +1,43 @@
 <template>
-  <div>{{ projectIssues }}</div>
+  <div class="project-issues">
+    <q-list>
+      <BaseItem
+        v-for="item in projectIssues"
+        :key="item.id"
+        class="flex-center-between"
+        @click="openIssueDetail(item.id)"
+      >
+        {{ item.name }}
+
+        <div class="flex gap-2">
+          <ProjectBoardIconIssuePriority :priority="item.priorityID" />
+          <ProjectBoardIconIssueType :type="item.typeID" />
+        </div>
+      </BaseItem>
+    </q-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onBeforeMount } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 import { IssueModel } from 'src/models/project/issue.model';
-
 import projectRepository from 'src/repositories/projectRepository';
+
+import ProjectBoardIconIssuePriority from 'components/project/board/icon/ProjectBoardIconIssuePriority.vue';
+import ProjectBoardIconIssueType from 'components/project/board/icon/ProjectBoardIconIssueType.vue';
 
 export default defineComponent({
   name: 'ProjectIssues',
 
+  components: {
+    ProjectBoardIconIssuePriority,
+    ProjectBoardIconIssueType,
+  },
+
   setup() {
+    const router = useRouter();
     const route = useRoute();
 
     const projectIssues = ref<IssueModel[]>();
@@ -22,9 +46,20 @@ export default defineComponent({
       projectIssues.value = await projectRepository.getIssuesByProjectID(projectID);
     });
 
+    async function openIssueDetail(issueID: number) {
+      await router.push(`/projects/${Number(route.params.projectID)}/issues/${issueID}`);
+    }
+
     return {
       projectIssues,
+      openIssueDetail,
     };
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.project-issues {
+  width: 300px;
+}
+</style>
