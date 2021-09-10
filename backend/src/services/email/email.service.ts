ff997299301cbbb5cedd01f0fd4160e93b8e867d
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-10 20:24:58 +0500

    b-email: fix email service transport options fields

--- backend/src/services/email/email.service.ts
+++ backend/src/services/email/email.service.ts
@@ -8,10 +8,11 @@ export class EmailService {
 
   constructor() {
     this.nodemailerTransport = createTransport({
-      service: process.env.EMAIL_SERVICE,
+      host: process.env.EMAIL_SERVICE,
+      port: 2525,
       auth: {
         user: process.env.EMAIL_USER,
-        password: process.env.EMAIL_PASSWORD,
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
   }
