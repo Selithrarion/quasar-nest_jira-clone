c08ef9756c3cc81a022c31207942b00d0ebbcc94
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-11 15:08:46 +0500

    b-email-verification: import email and jwt modules

--- backend/src/modules/email-verification/email-verification.module.ts
+++ backend/src/modules/email-verification/email-verification.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { EmailVerificationService } from './email-verification.service';
 import { EmailVerificationController } from './email-verification.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { EmailModule } from '../../services/email/email.module';
 
 @Module({
+  imports: [
+    EmailModule,
+    JwtModule.registerAsync({
+      useFactory: async () => ({
+        secret: process.env.JWT_EMAIL_VERIFICATION_SECRET,
+        signOptions: { expiresIn: process.env.JWT_EMAIL_VERIFICATION_EXPIRES_IN },
+      }),
+    }),
+  ],
   providers: [EmailVerificationService],
-  controllers: [EmailVerificationController]
+  controllers: [EmailVerificationController],
 })
 export class EmailVerificationModule {}
