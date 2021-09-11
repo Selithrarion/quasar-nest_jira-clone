8979df31d9cc54a2882bc3ea77799a2c27209f3d
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-11 17:35:52 +0500

    b-email-verification: add confirm and resend endpoints

--- backend/src/modules/email-verification/email-verification.controller.ts
+++ backend/src/modules/email-verification/email-verification.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Query, Request } from '@nestjs/common';
+import { EmailVerificationService } from './email-verification.service';
+import { Public } from '../auth/decorators/public.decorator';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('email-verification')
 @Controller('email-verification')
-export class EmailVerificationController {}
+export class EmailVerificationController {
+  constructor(private readonly emailVerificationService: EmailVerificationService) {}
+
+  @ApiOperation({ summary: 'Confirm email with token' })
+  @ApiResponse({ status: 200, description: 'Email was confirmed' })
+  @ApiResponse({ status: 400, description: 'Token is expired or incorrect' })
+  @Public()
+  @Post()
+  async confirm(@Query('token') token: string): Promise<boolean> {
+    const email = await this.emailVerificationService.confirmToken(token);
+    return await this.emailVerificationService.confirmEmail(email);
+  }
+
+  @ApiOperation({ summary: 'Resend confirmation email' })
+  @ApiResponse({ status: 200, description: 'Confirmation email was resend' })
+  @Public()
+  @Post('resend')
+  resend(@Request() req): Promise<void> {
+    return this.emailVerificationService.resend(req.user.id);
+  }
+}
--- backend/src/modules/email-verification/email-verification.service.ts
+++ backend/src/modules/email-verification/email-verification.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { EmailService } from '../../services/email/email.service';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class EmailVerificationService {
-  constructor(private readonly jwtService: JwtService, private readonly emailService: EmailService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly emailService: EmailService,
+    private readonly userService: UserService
+  ) {}
 
   sendVerificationLink(email: string): Promise<void> {
     const token = this.jwtService.sign({ email });
 
-    const isLocalhost = process.env.SERVER_URL === 'localhost';
-    const localHostURL = `${process.env.SERVER_URL}:${process.env.PORT}`;
-    const url = isLocalhost ? `${localHostURL}?token=${token}` : `${process.env.SERVER_URL}?token=${token}`;
-
+    const url = `${process.env.SERVER_URL}?token=${token}`;
     const subject = 'Jira email confirmation';
     const text = `Welcome to Jira (clone). To confirm the email address, click here: ${url}`;
 
@@ -21,5 +23,28 @@ export class EmailVerificationService {
       subject,
       text,
     });
+  }
+
+  async confirmToken(token: string): Promise<string> {
+    try {
+      const payload = await this.jwtService.verify(token);
+      if (payload.email) return payload.email;
+    } catch (e) {
+      const isExpiredError = e.name === 'TokenExpiredError';
+      if (isExpiredError) throw new BadRequestException('EMAIL_CONFIRMATION_TOKEN_EXPIRED');
+      throw new BadRequestException('EMAIL_CONFIRMATION_TOKEN_INCORRECT');
+    }
+  }
+  async confirmEmail(email: string): Promise<boolean> {
+    const user = await this.userService.getByEmail(email);
+    if (user.isEmailConfirmed) throw new BadRequestException('EMAIL_ALREADY_CONFIRMED');
+    return await this.userService.markEmailAsConfirmed(email);
+  }
+
+  // TODO: need to invalidate previous token (store previous token in database and compare)
+  async resend(userID: number): Promise<void> {
+    const user = await this.userService.getByID(userID);
+    if (user.isEmailConfirmed) throw new BadRequestException('EMAIL_ALREADY_CONFIRMED');
+    return await this.sendVerificationLink(user.email);
   }
 }
