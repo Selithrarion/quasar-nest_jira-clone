8bb218e3e9a38b93053369c1e65fb3f9b7e5b5b1
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-11 15:09:03 +0500

    b-email-verification: add sendVerificationLink

--- backend/src/modules/email-verification/email-verification.service.ts
+++ backend/src/modules/email-verification/email-verification.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { EmailService } from '../../services/email/email.service';
 
 @Injectable()
-export class EmailVerificationService {}
+export class EmailVerificationService {
+  constructor(private readonly jwtService: JwtService, private readonly emailService: EmailService) {}
+
+  sendVerificationLink(email: string): Promise<void> {
+    const token = this.jwtService.sign({ email });
+
+    const isLocalhost = process.env.SERVER_URL === 'localhost';
+    const localHostURL = `${process.env.SERVER_URL}:${process.env.PORT}`;
+    const url = isLocalhost ? `${localHostURL}?token=${token}` : `${process.env.SERVER_URL}?token=${token}`;
+
+    const subject = 'Jira email confirmation';
+    const text = `Welcome to Jira (clone). To confirm the email address, click here: ${url}`;
+
+    return this.emailService.sendEmail({
+      to: email,
+      subject,
+      text,
+    });
+  }
+}
