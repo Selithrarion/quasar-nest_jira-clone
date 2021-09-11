5e83e0abb39295a8d929d6d57b76ee27e2a812c8
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-11 16:13:11 +0500

    b-auth: remove if () brackets

--- backend/src/modules/auth/guard/jwt-auth.guard.ts
+++ backend/src/modules/auth/guard/jwt-auth.guard.ts
@@ -15,9 +15,8 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       context.getHandler(),
       context.getClass(),
     ]);
-    if (isPublic) {
-      return true;
-    }
+    if (isPublic) return true;
+
     return super.canActivate(context);
   }
 }
