79d43f89861d11c1f2197f0569ed6fdcc5a8b3d5
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-16 10:16:10 +0500

    b-auth: add google auth logic

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,17 +1,33 @@
 import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
-import { UserEntity, UserTokensInterface, UserUpdateTokensDTO, UserValidationDTO } from '../user/entity/user.entity';
+
+import {
+  UserEntity,
+  UserGoogleData,
+  UserTokensInterface,
+  UserUpdateTokensDTO,
+  UserValidationDTO,
+} from '../user/entity/user.entity';
 import { CreateUserDTO } from '../user/dto';
+
+import { google, Auth } from 'googleapis';
 import { EmailVerificationService } from '../email-verification/email-verification.service';
 
 @Injectable()
 export class AuthService {
+  oauthClient: Auth.OAuth2Client;
+
   constructor(
     private readonly userService: UserService,
     private readonly emailVerificationService: EmailVerificationService,
     private readonly jwtService: JwtService
-  ) {}
+  ) {
+    const googleClientID = process.env.GOOGLE_AUTH_CLIENT_ID;
+    const googleClientSecret = process.env.GOOGLE_AUTH_CLIENT_SECRET;
+
+    this.oauthClient = new google.auth.OAuth2(googleClientID, googleClientSecret);
+  }
 
   async validateUser({ email, password }: UserValidationDTO): Promise<UserEntity> {
     const user = await this.userService.getByEmail(email);
@@ -26,6 +42,7 @@ export class AuthService {
     const payload = { id: user.id, email: user.email };
     const accessToken = await this.jwtService.sign(payload);
     console.log('AUTH SERV login t, p', accessToken, user);
+    //   await this.userService.setRefreshToken(refreshToken, userID);
     return {
       user,
       accessToken,
@@ -46,10 +63,42 @@ export class AuthService {
     const accessToken = this.jwtService.sign({ id: userID, email });
     console.log('UPDATE TOKENS', accessToken, refreshToken, userID);
     console.log(this.jwtService.verify(accessToken));
+    //   await this.userService.setRefreshToken(refreshToken, userID);
     return {
       user,
       accessToken,
       refreshToken: 'TEST',
+    };
+  }
+
+  async authWithGoogle(token: string): Promise<UserTokensInterface> {
+    const { email } = await this.oauthClient.getTokenInfo(token);
+    const user = await this.userService.getByEmail(email);
+
+    if (user) return this.login(user);
+    return this.registerWithGoogle(token, email);
+  }
+
+  async registerWithGoogle(token: string, email: string): Promise<UserTokensInterface> {
+    const { picture } = await this.getGoogleUserData(token);
+    const user = await this.userService.createWithGoogle(email);
+    return this.login(user);
+  }
+
+  async getGoogleUserData(token: string): Promise<UserGoogleData> {
+    const userClient = google.oauth2('v2').userinfo;
+    this.oauthClient.setCredentials({
+      access_token: token,
+    });
+
+    const { data } = await userClient.get({
+      auth: this.oauthClient,
+    });
+
+    // TODO: need to get google username and name
+    return {
+      email: data.email,
+      picture: data.picture,
     };
   }
 }
