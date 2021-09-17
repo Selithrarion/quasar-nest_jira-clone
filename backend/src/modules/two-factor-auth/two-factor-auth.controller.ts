04854676676c1ab21b9a9c76372e742889ce5a19
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-17 09:55:57 +0500

    b-2fa: add enable endpoint

--- backend/src/modules/two-factor-auth/two-factor-auth.controller.ts
+++ backend/src/modules/two-factor-auth/two-factor-auth.controller.ts
@@ -1,19 +1,31 @@
-import { Controller, Post, Request, Response } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Request, Response, UnauthorizedException } from '@nestjs/common';
 import { TwoFactorAuthService } from './two-factor-auth.service';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserService } from '../user/user.service';
 
 @ApiTags('2fa')
 @Controller('2fa')
 export class TwoFactorAuthController {
-  constructor(private readonly twoFactorAuthService: TwoFactorAuthService) {}
+  constructor(private readonly twoFactorAuthService: TwoFactorAuthService, private readonly userService: UserService) {}
 
   @ApiOperation({ summary: 'Generate QR-code' })
   @ApiResponse({ status: 200, description: 'Generated' })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiBearerAuth()
   @Post('generate')
-  async register(@Response() response, @Request() request): Promise<void> {
+  async generate(@Response() response, @Request() request): Promise<void> {
     const { otpURL } = await this.twoFactorAuthService.generateSecret(request.user.id);
     return this.twoFactorAuthService.pipeQrCodeStream(response, otpURL);
+  }
+
+  @Post('enable')
+  @HttpCode(200)
+  async enable(@Body('code') code: string, @Request() request): Promise<void> {
+    const userID = request.user.id;
+
+    const isValid = this.twoFactorAuthService.isValid(code, userID);
+    if (!isValid) throw new UnauthorizedException('WRONG_OTP_CODE');
+
+    await this.userService.enable2FA(userID);
   }
 }
