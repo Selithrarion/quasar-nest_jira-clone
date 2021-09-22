bb1e40db50f4fbd8b839cb284c1ff4cf498e8b16
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-22 11:00:38 +0500

    b-2fa: add 2fa auth endpoint, add docs

--- backend/src/modules/two-factor-auth/two-factor-auth.controller.ts
+++ backend/src/modules/two-factor-auth/two-factor-auth.controller.ts
@@ -1,31 +1,53 @@
-import { Body, Controller, HttpCode, Post, Request, Response, UnauthorizedException } from '@nestjs/common';
-import { TwoFactorAuthService } from './two-factor-auth.service';
+import { BadRequestException, Body, Controller, HttpCode, Post, Request } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { TwoFactorAuthService } from './two-factor-auth.service';
+import { AuthService } from '../auth/auth.service';
 import { UserService } from '../user/user.service';
+import { UserTokensInterface } from '../user/entity/user.entity';
+import { Public } from '../auth/decorators/public.decorator';
 
 @ApiTags('2fa')
 @Controller('2fa')
 export class TwoFactorAuthController {
-  constructor(private readonly twoFactorAuthService: TwoFactorAuthService, private readonly userService: UserService) {}
+  constructor(
+    private readonly twoFactorAuthService: TwoFactorAuthService,
+    private readonly authService: AuthService,
+    private readonly userService: UserService
+  ) {}
 
   @ApiOperation({ summary: 'Generate QR-code' })
   @ApiResponse({ status: 200, description: 'Generated' })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiBearerAuth()
   @Post('generate')
-  async generate(@Response() response, @Request() request): Promise<void> {
-    const { otpURL } = await this.twoFactorAuthService.generateSecret(request.user.id);
-    return this.twoFactorAuthService.pipeQrCodeStream(response, otpURL);
+  async generate(@Request() req): Promise<string> {
+    const { otpURL } = await this.twoFactorAuthService.generateSecret(req.user);
+    return this.twoFactorAuthService.getQrCodeURL(otpURL);
   }
 
+  @ApiOperation({ summary: 'Enable 2FA' })
+  @ApiResponse({ status: 200, description: 'Enabled' })
+  @ApiResponse({ status: 500, description: 'Wrong OTP code' })
+  @ApiBearerAuth()
   @Post('enable')
   @HttpCode(200)
-  async enable(@Body('code') code: string, @Request() request): Promise<void> {
-    const userID = request.user.id;
+  async enable(@Body('code') code: string, @Request() req): Promise<boolean> {
+    const isValid = await this.twoFactorAuthService.isValid(code, req.user);
+    if (!isValid) throw new BadRequestException('WRONG_OTP_CODE');
 
-    const isValid = this.twoFactorAuthService.isValid(code, userID);
-    if (!isValid) throw new UnauthorizedException('WRONG_OTP_CODE');
+    return await this.userService.enable2FA(req.user.id);
+  }
 
-    await this.userService.enable2FA(userID);
+  @ApiOperation({ summary: 'Auth with 2FA' })
+  @ApiResponse({ status: 200, description: 'Successfully logged in' })
+  @ApiResponse({ status: 500, description: 'Wrong OTP code' })
+  @ApiBearerAuth()
+  @Post('auth')
+  @HttpCode(200)
+  async auth(@Body('code') code: string, @Request() req): Promise<UserTokensInterface> {
+    const isValid = await this.twoFactorAuthService.isValid(code, req.user);
+    if (!isValid) throw new BadRequestException('WRONG_OTP_CODE');
+
+    return this.authService.login(req.user, true);
   }
 }
