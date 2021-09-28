344901962e4ac38fbd17a08b8a8cbe8a4842b331
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-09-28 11:30:51 +0500

    f-people: add adding users to team

--- frontend/src/components/people/detail/PeopleDetailSideTeam.vue
+++ frontend/src/components/people/detail/PeopleDetailSideTeam.vue
@@ -6,15 +6,17 @@
     :name="team.name"
     :color="team.color"
     :show-more-button="canEdit"
+    :can-edit="canEdit"
     @button-click="dialog.open('addUsers')"
+    @update:display-name="updateTeamInfo('name', $event)"
   >
     <template #moreButtonItems>
-      <BaseItem v-if="canEdit" label="Удалить команду" />
+      <BaseItem v-if="canEdit" label="Удалить команду" @click="dialog.open('deleteTeam')" />
     </template>
 
     <template #append>
       <q-card>
-        <q-card-section class="row gap-2">
+        <q-card-section class="column gap-2">
           <CommonListTitle title="участники" />
           <BaseItem v-for="user in team.users" :key="user.id" @click="openUserProfile(user.id)">
             <q-item-section side>
@@ -30,18 +32,30 @@
 
       <BaseDialog
         v-if="dialog.openedName.value === 'addUsers'"
-        title="Добавть участников в команду"
+        title="Добавить участников в команду"
+        :confirm-loading="dialog.loading.value"
         @close="dialog.close"
         @confirm="addUsersToTeam"
       >
-        <CommonSelectUsers v-model="localAddedTeamUsers" v-model:options="availableUsers" />
+        <!--TODO: hide already added users-->
+        <CommonSelectUsers v-model="form.users" />
+      </BaseDialog>
+      <BaseDialog
+        v-if="dialog.openedName.value === 'deleteTeam'"
+        type="delete"
+        title="Удалить команду?"
+        :confirm-loading="dialog.loading.value"
+        @close="dialog.close"
+        @confirm="deleteTeam"
+      >
+        Удалить команду <span class="text-weight-medium">{{ team.name }}</span> с {{ team.users.length }} участниками?
       </BaseDialog>
     </template>
   </PeopleDetailSide>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed, PropType } from 'vue';
+import { defineComponent, reactive, PropType } from 'vue';
 import { useRouter } from 'vue-router';
 import { useStore } from 'src/store';
 import useDialog from 'src/composables/common/useDialog';
@@ -49,6 +63,8 @@ import useDialog from 'src/composables/common/useDialog';
 import CommonListTitle from 'components/common/CommonListTitle.vue';
 import CommonSelectUsers from 'components/common/CommonSelectUsers.vue';
 import PeopleDetailSide from 'components/people/detail/PeopleDetailSide.vue';
+
+import teamRepository from 'src/repositories/teamRepository';
 
 import { TeamModel } from 'src/models/user/team.model';
 
@@ -69,15 +85,40 @@ export default defineComponent({
     canEdit: Boolean,
   },
 
-  setup() {
+  setup(props) {
     const router = useRouter();
     const store = useStore();
     const dialog = useDialog();
 
-    const availableUsers = computed(() => store.state.people.availableUsers);
-    const localAddedTeamUsers = ref([]);
     async function addUsersToTeam() {
-      //
+      try {
+        dialog.startLoading();
+        await teamRepository.addUsers(props.team.id, form.users);
+      } finally {
+        dialog.stopLoading();
+      }
+    }
+
+    const form = reactive({
+      users: [],
+    });
+
+    async function updateTeamInfo<T>(field: string, value: T) {
+      const id = props.team.id;
+      const payload = {
+        [field]: value,
+      };
+
+      await store.dispatch('people/updateTeam', { id, payload });
+    }
+    async function deleteTeam() {
+      try {
+        dialog.startLoading();
+        await teamRepository.delete(props.team.id);
+        await router.push('/people');
+      } finally {
+        dialog.stopLoading();
+      }
     }
 
     async function openUserProfile(userID: number) {
@@ -87,9 +128,11 @@ export default defineComponent({
     return {
       dialog,
 
-      availableUsers,
-      localAddedTeamUsers,
+      form,
       addUsersToTeam,
+
+      updateTeamInfo,
+      deleteTeam,
 
       openUserProfile,
     };
