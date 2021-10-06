a8a3a287e4f4407b04d774544fe7afc99239d12d
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-10-06 08:56:00 +0500

    b-utils: fix stringToHslColor slice

--- backend/src/common/utils/stringToHslColor.ts
+++ backend/src/common/utils/stringToHslColor.ts
@@ -1,6 +1,6 @@
 export default function stringToHslColor(string: string, saturation = 50, lightness = 80): string {
   let hash = 0;
-  const shortString = string.slice(0, 10);
+  const shortString = string.slice(0, 100);
 
   for (let i = 0; i < shortString.length; i++) {
     hash = string.charCodeAt(i) + ((hash << 5) - hash);
