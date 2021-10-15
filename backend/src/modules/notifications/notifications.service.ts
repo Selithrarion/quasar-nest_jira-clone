ec1fb4e99155e16e1ae6fa9fe64b162e4bf34b5b
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-10-15 19:15:12 +0500

    b-notifications: add create method

--- backend/src/modules/notifications/notifications.service.ts
+++ backend/src/modules/notifications/notifications.service.ts
@@ -2,8 +2,9 @@ import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { NotificationEntity } from './entity/notification.entity';
+import { NotificationEntity, NotificationTypes } from './entity/notification.entity';
 import { UserService } from '../user/user.service';
+import { UserEntity } from '../user/entity/user.entity';
 
 @Injectable()
 export class NotificationsService {
@@ -18,6 +19,11 @@ export class NotificationsService {
   async getAll(userID: number): Promise<NotificationEntity[]> {
     const userNotification = this.userService.getNotifications(userID);
     return userNotification;
+  }
+
+  async create(typeID: NotificationTypes, user: UserEntity): Promise<NotificationEntity> {
+    const item = this.notifications.create({ typeID, user });
+    return await this.notifications.save(item);
   }
 
   async readByID(id: number): Promise<void> {
