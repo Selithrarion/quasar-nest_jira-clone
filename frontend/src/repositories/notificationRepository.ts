f147093e8e088f9ff3e7f3762df8f2b1e95920ea
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-10-20 12:30:49 +0500

    f-notifications: add toggle read notification api method, fix endpoint path

--- frontend/src/repositories/notificationRepository.ts
+++ frontend/src/repositories/notificationRepository.ts
@@ -4,13 +4,17 @@ import { NotificationModel } from 'src/models/user/notification.model';
 
 export default {
   async getAll(): Promise<NotificationModel[]> {
-    const { data }: ApiResponseModel<NotificationModel[]> = await http.get('/notification');
+    const { data }: ApiResponseModel<NotificationModel[]> = await http.get('/notifications');
     return data;
   },
   async readByID(id: number): Promise<void> {
-    await http.patch(`/notification/${id}`);
+    await http.patch(`/notifications/read/${id}`);
   },
+  async toggleRead(id: number): Promise<void> {
+    await http.patch(`/notifications/toggle-read/${id}`);
+  },
+
   async delete(id: number): Promise<void> {
-    return await http.delete(`/notification/${id}`);
+    return await http.delete(`/notifications/${id}`);
   },
 };
