ce2fa4d7efb489c28134e59fd0679c0e58229089
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-10-20 07:31:00 +0500

    b-notifications: fix no export and no user module import

--- backend/src/modules/notifications/notifications.module.ts
+++ backend/src/modules/notifications/notifications.module.ts
@@ -5,8 +5,11 @@ import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { NotificationEntity } from './entity/notification.entity';
 
+import { UserModule } from '../user/user.module';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([NotificationEntity])],
+  imports: [TypeOrmModule.forFeature([NotificationEntity]), UserModule],
+  exports: [NotificationsService],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
