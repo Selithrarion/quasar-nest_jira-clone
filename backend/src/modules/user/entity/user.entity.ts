6229668cead33b9b3bf995cd94c5f329f9b514b6
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-12-05 13:26:35 +0500

    b-user: add index to username and email columns

--- backend/src/modules/user/entity/user.entity.ts
+++ backend/src/modules/user/entity/user.entity.ts
@@ -2,6 +2,7 @@ import {
   BeforeInsert,
   Column,
   Entity,
+  Index,
   JoinColumn,
   JoinTable,
   ManyToMany,
@@ -58,10 +59,14 @@ export interface UserJwtPayload {
 export class UserEntity extends BaseEntity {
   @Column({ length: 64 })
   name: string;
+
   @Column({ length: 24, unique: true })
+  @Index()
   username: string;
+
   @Column({ unique: true })
   @IsEmail()
+  @Index()
   email: string;
 
   @Exclude()
